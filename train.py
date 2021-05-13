22727db75931d03d0b44a173f2d44f74f5340f84
Karthik Iyer <kni6228@rit.edu>
2021-05-13 10:47:26 -0400

    nits

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 import torch
 import torch.nn as nn
@@ -98,10 +99,10 @@ def main():
                 params_to_learn.append(param)
     optimizer = optim.SGD(params_to_learn, lr=0.1, momentum=0.9)
 
-    model, train_acc_history, val_acc_history = train(model, dataloaders, criterion, optimizer, num_epochs=50)
+    model, train_acc_history, val_acc_history = train(model, dataloaders, criterion, optimizer, num_epochs=25)
     torch.save(model, MODEL_PATH)
-    plot_learning_curve({'train': train_acc_history, 'val':val_acc_history}, num_epochs=50)
-    test(model, dataloaders, criterion, 'test')
+    plot_learning_curve({'train': train_acc_history, 'val': val_acc_history}, num_epochs=25)
+    # test(model, dataloaders, criterion, 'test')
 
 
 if __name__ == '__main__':
--- visuals.py
+++ visuals.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import os
 from constants import LEARNING_CURVE_PATH
 
 
-def plot_learning_curve(phase_histories, num_epochs=20):
+def plot_learning_curve(train_acc_history, val_acc_history, num_epochs=20):
+    if not os.path.exists("./graphs"):
+       os.mkdir("./graphs")
+
     plt.title('Learning Curve')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy')
     for phase, history in phase_histories.items():
         plt.plot(range(1, num_epochs + 1), history, label=phase)
     plt.ylim((0, 1.))
+    plt.yticks(np.arange(0.1, 1.0, 0.1))
     plt.xticks(np.arange(1, num_epochs + 1, np.ceil(num_epochs / 20)))
     plt.savefig(LEARNING_CURVE_PATH)
     plt.show()
